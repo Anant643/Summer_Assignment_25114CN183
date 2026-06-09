23135075ae2df3e5b84bf5bdb6d2734fd9c8c59e
Anant643 <anantsharma6034@gmail.com>
2026-06-09 22:39:52 +0530

    Add files via upload

---
+++ Week_2/Day09.c/Q34.c
@@ -0,0 +1,27 @@
+/*
+Write a program to Print reverse number
+triangle.
+12345
+1234
+123
+12
+1
+*/
+
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    int n = 6;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= n - i; j++)
+        {
+            printf("%d",j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ Week_2/Day09.c/Q35.c
@@ -0,0 +1,27 @@
+/*
+ Write a program to Print repeated character
+pattern.
+A
+BB
+CCC
+DDDD
+EEEEE
+*/
+
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    int n = 6;
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%c", 'A'+i-1);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
