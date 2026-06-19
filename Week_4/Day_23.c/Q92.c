56858ca26d59442692c36d28e97b636773d07dc5
Anant643 <anantsharma6034@gmail.com>
2026-06-19 21:40:48 +0530

    Add files via upload

---
+++ Week_4/Day_23.c/Q92.c
@@ -0,0 +1,28 @@
+//Find Maximum Occurring Character
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int freq[256] = {0};
+    int i, max = 0;
+    char maxChar;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        freq[(int)str[i]]++;
+    }
+
+    for (i = 0; i < 256; i++) {
+        if (freq[i] > max) {
+            max = freq[i];
+            maxChar = i;
+        }
+    }
+
+    printf("Maximum occurring character: %c\n", maxChar);
+    printf("Frequency: %d\n", max);
+
+    return 0;
+}
